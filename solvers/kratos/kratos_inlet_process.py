396b3d3c8e344eb483240dddfd52e7297f713122
msandre <michael.andre@tum.de>
2019-05-23 17:25:49 +0200

    feat: added kratos inlet process

---
+++ solvers/kratos/kratos_inlet_process.py
@@ -0,0 +1,301 @@
+'''An inlet boundary condition process for KratosMultiphysics
+
+license: license.txt
+'''
+
+__all__ = ['Factory', 'ImposeWindInletProcess']
+
+from collections import namedtuple
+from collections import Mapping
+from math import isclose
+from math import floor
+from math import ceil
+from math import log
+
+import h5py
+
+import KratosMultiphysics
+from KratosMultiphysics import TIME
+from KratosMultiphysics import DELTA_TIME
+from KratosMultiphysics import VELOCITY_X
+from KratosMultiphysics import VELOCITY_Y
+from KratosMultiphysics import VELOCITY_Z
+
+
+class Parameters(Mapping):
+
+    def __init__(self, kratos_parameters):
+        self._kratos_parameters = kratos_parameters
+
+    def __getitem__(self, key):
+        param = self._kratos_parameters[key]
+        if param.IsDouble():
+            value = param.GetDouble()
+        elif param.IsInt():
+            value = param.GetInt()
+        elif param.IsString():
+            value = param.GetString()
+        else:
+            value = param
+        return value
+
+    def __iter__(self):
+        yield from self._kratos_parameters.keys()
+
+    def __len__(self):
+        return self._kratos_parameters.size()
+
+        
+def Factory(settings, Model):
+    return ImposeWindInletProcess(Model, Parameters(settings['Parameters']))
+
+
+Extent = namedtuple('Extent', ['lower', 'upper'])
+
+
+class RegularGrid1D:
+
+    @property
+    def lower_bound(self):
+        return self.extent.lower
+
+    @property
+    def upper_bound(self):
+        return self.extent.upper
+
+    def __init__(self, start_pos, length, size):
+        self.extent = Extent(start_pos, start_pos+length)
+        self.size = size
+        self.step_size = (self.upper_bound-self.lower_bound) / (self.size-1)
+
+    def __getitem__(self, index):
+        return self.lower_bound + self.step_size*index
+
+    def __len__(self):
+        return self.size
+
+    def floor_index(self, coord):
+        if isclose(coord, self.lower_bound, abs_tol=1e-4):
+            # Guard against negative index due to floating point representation.
+            return 0
+        local_coord = coord - self.lower_bound
+        return int(floor(local_coord / self.step_size))
+
+    def ceil_index(self, coord):
+        if isclose(coord, self.upper_bound, abs_tol=1e-4):
+            return len(self) - 1
+        local_coord = coord - self.lower_bound
+        return int(ceil(local_coord / self.step_size))
+
+    def has(self, coord):
+        return (self.floor_index(coord) >= 0
+                and self.ceil_index(coord) < len(self))
+
+
+IndexSpan = namedtuple('IndexSpan', ['begin', 'end'])
+
+
+class SubGrid1D:
+
+    @property
+    def lower_bound(self):
+        return self.grid[self.span.begin]
+
+    @property
+    def upper_bound(self):
+        return self.grid[self.span.end]
+
+    @property
+    def step_size(self):
+        return self.grid.step_size
+
+    @property
+    def gslice(self):
+        return slice(self.span.begin, self.span.end+1)
+
+    def __init__(self, grid, start_pos, end_pos):
+        self.grid = grid
+        start_index = grid.floor_index(start_pos)
+        end_index = grid.ceil_index(end_pos)
+        end_index = max(end_index, start_index+1)
+        self.span = IndexSpan(start_index, end_index)
+        
+    def __getitem__(self, index):
+        return self.grid[self.span.begin + index]
+
+    def __len__(self):
+        return self.span.end - self.span.begin + 1
+
+    def has(self, coord):
+        return (self.floor_index(coord) >= 0
+                and self.ceil_index(coord) < len(self))
+
+    def floor_index(self, coord):
+        if isclose(coord, self.lower_bound, abs_tol=1e-4):
+            return 0
+        return self.grid.floor_index(coord) - self.span.begin
+
+    def ceil_index(self, coord):
+        if isclose(coord, self.upper_bound, abs_tol=1e-4):
+            return len(self) - 1
+        return self.grid.ceil_index(coord) - self.span.begin
+
+
+Grid3D = namedtuple('Grid3D', ['x', 'y', 'z'])
+
+
+class InletPanel3D:
+
+    def __init__(self, grid, data):
+        self.grid = grid
+        self.data = data
+        self.dy = self.grid.y.step_size
+        self.dz = self.grid.z.step_size
+        self.y0 = self.grid.y.lower_bound
+        self.z0 = self.grid.z.lower_bound
+
+    def update(self, pos):
+        i = self.grid.x.floor_index(pos)
+        tx = (pos-self.grid.x[i]) / self.grid.x.step_size
+        data_0 = self.data[i, self.grid.y.gslice, self.grid.z.gslice]
+        data_1 = self.data[i+1, self.grid.y.gslice, self.grid.z.gslice]
+        self.cut_data = (1.0 - tx) * data_0 + tx * data_1
+
+    def interpolate(self, node):
+        # xi and eta are local mesh cell coordinates in the interval [-1, 1].
+        xi = ((node.Y - self.y0) % self.dy) / self.dy
+        eta = ((node.Z - self.z0) % self.dz) / self.dz
+        xi = 2.0 * (xi-0.5)
+        eta = 2.0 * (eta-0.5)
+        # Interpolate using bilinear shape functions.
+        weights = (
+            0.25 * (1.0-xi) * (1.0-eta),
+            0.25 * (1.0+xi) * (1.0-eta),
+            0.25 * (1.0+xi) * (1.0+eta),
+            0.25 * (1.0-xi) * (1.0+eta)
+        )
+        j = self.grid.y.floor_index(node.Y)
+        k = self.grid.z.floor_index(node.Z)
+        return (
+            weights[0] * self.cut_data[j, k]
+            + weights[1] * self.cut_data[j+1, k]
+            + weights[2] * self.cut_data[j+1, k+1]
+            + weights[3] * self.cut_data[j, k+1]
+        )
+
+
+def weak_min(nodes, key): return key(min(nodes, key=key))
+
+
+def weak_max(nodes, key): return key(max(nodes, key=key))
+
+
+def get_extent(nodes, key):
+    lower_bound = weak_min(nodes, key=key)
+    upper_bound = weak_max(nodes, key=key)
+    return Extent(lower_bound, upper_bound)
+
+
+class LogMeanProfile:
+
+    def __init__(self, friction_velocity, log_z0, bulk_wind_speed,
+                 key=lambda node: node.Z):
+        self.friction_velocity = friction_velocity
+        self.log_z0 = log_z0
+        self.bulk_wind_speed = bulk_wind_speed
+        self.key = key
+            
+    def wind_speed(self, node):
+        return (2.439 * self.friction_velocity
+                * log((self.key(node) + self.log_z0) / self.log_z0))
+
+
+class ImposeWindInletProcess:
+
+    @property
+    def inlet_nodes(self):
+        return self.model_part.Nodes
+
+    def __init__(self, Model, settings):
+        for name, value in settings.items():
+            setattr(self, name, value)
+        with self.OpenFile() as file_:
+            for key, value in file_.items():
+                if key in ['lx', 'ly', 'lz', 'log_z0', 'height', 'umean']:
+                    setattr(self, key, value[0])
+        self.model_part = Model[self.inlet_model_part_name]
+        self.mean_profile = self.CreateLogMeanProfile()
+        if len(self.inlet_nodes) > 0:
+            # In MPI we pin the file if the process has nodes on the inlet.
+            # The mappers are only valid during the lifetime of the file.
+            self.file_ = self.OpenFile()
+            self.mappers = self.Create3DMappers(self.file_)
+
+    def ExecuteInitialize(self):
+        for node in self.inlet_nodes:
+            for var, _ in self.mappers:
+                node.Fix(var)
+        
+    def ExecuteInitializeSolutionStep(self):
+        self.UpdateInletPosition()
+        Logger.PrintInfo('ImposeWindInletProcess',
+                         'inlet position = %e' % self.inlet_position)
+        self.AssignVelocity()
+        self.ApplyRamp()
+
+    def OpenFile(self):
+        return h5py.File(self.wind_filename, 'r')
+        
+    def CreateLogMeanProfile(self, key=lambda node: node.Z):
+        z = self.height
+        lz = self.lz
+        log_z0 = self.log_z0
+        umean = self.umean
+        bulk_wind_speed = umean * (log(lz/log_z0) - 1.0) / log(z/log_z0)
+        friction_velocity = umean * 0.41 / log((z + log_z0) / log_z0)
+        return LogMeanProfile(friction_velocity, log_z0, bulk_wind_speed)
+
+    def Create3DMappers(self, file_):
+        nx, ny, nz = file_['u'].shape
+        x_grid = RegularGrid1D(0., self.lx, nx)
+        y_grid = RegularGrid1D(self.y0, self.ly, ny)
+        z_grid = RegularGrid1D(self.z0, self.lz, nz)
+        y_extent = get_extent(self.inlet_nodes, key=lambda node: node.Y)
+        z_extent = get_extent(self.inlet_nodes, key=lambda node: node.Z)
+        y_subgrid = SubGrid1D(y_grid, y_extent.lower, y_extent.upper)
+        z_subgrid = SubGrid1D(z_grid, z_extent.lower, z_extent.upper)
+        grid = Grid3D(x_grid, y_subgrid, z_subgrid)
+        # mappers become invalid after file_ is destructed.
+        mappers = ((VELOCITY_X, InletPanel3D(grid, file_['u'])),
+                   (VELOCITY_Y, InletPanel3D(grid, file_['v'])),
+                   (VELOCITY_Z, InletPanel3D(grid, file_['w'])))
+        return mappers
+
+    def UpdateInletPosition(self):
+        dt = self.model_part.ProcessInfo[DELTA_TIME]
+        self.inlet_position += dt * self.mean_profile.bulk_wind_speed
+        # Use the property that the wind data is periodic to cycle the domain.
+        if self.inlet_position >= self.lx:
+            self.inlet_position -= self.lx
+
+    def AssignVelocity(self):
+        for var, mapper in self.mappers:
+            mapper.update(self.inlet_position)
+            if var == VELOCITY_X:
+                for node in self.inlet_nodes:
+                    vel = self.mean_profile.wind_speed(node)
+                    vel += mapper.interpolate(node)
+                    node.SetSolutionStepValue(var, vel)
+            else:
+                for node in self.inlet_nodes:
+                    vel = mapper.interpolate(node)
+                    node.SetSolutionStepValue(var, vel)
+
+    def ApplyRamp(self):
+        time = self.model_part.ProcessInfo[TIME]
+        if time < self.ramp_time:
+            scal = time / self.ramp_time
+            for node in self.inlet_nodes:
+                for var, _ in self.mappers:
+                    vel = node.GetSolutionStepValue(var)
+                    node.SetSolutionStepValue(var, scal * vel)
---
+++ solvers/kratos/test_kratos_inlet_process.py
@@ -0,0 +1,256 @@
+import unittest
+from unittest.mock import patch
+from unittest.mock import MagicMock
+from contextlib import AbstractContextManager
+import sys
+
+from numpy import array
+
+sys.modules['KratosMultiphysics'] = MagicMock()
+sys.modules['h5py'] = MagicMock()
+import kratos_inlet_process
+
+
+kratos_inlet_process.VELOCITY_X = 'VELOCITY_X'
+kratos_inlet_process.VELOCITY_Y = 'VELOCITY_Y'
+kratos_inlet_process.VELOCITY_Z = 'VELOCITY_Z'
+kratos_inlet_process.DELTA_TIME = 'DELTA_TIME'
+kratos_inlet_process.TIME = 'TIME'
+
+
+class TestRegularGrid1D(unittest.TestCase):
+
+    def setUp(self):
+        self.grid = kratos_inlet_process.RegularGrid1D(1., 1., 11)
+    
+    def test_getitem(self):
+        self.assertAlmostEqual(self.grid[0], 1.)
+        self.assertAlmostEqual(self.grid[1], 1.1)
+        self.assertAlmostEqual(self.grid[10], 2.)
+
+    def test_len(self):
+        self.assertEqual(len(self.grid), 11)
+
+    def test_floor_index(self):
+        self.assertEqual(self.grid.floor_index(0.999999), 0)
+        self.assertEqual(self.grid.floor_index(1.15), 1)
+
+    def test_ceil_index(self):
+        self.assertEqual(self.grid.ceil_index(2.000001), 10)
+        self.assertEqual(self.grid.ceil_index(1.95), 10)
+
+    def test_has(self):
+        self.assertFalse(self.grid.has(0.5))
+        self.assertTrue(self.grid.has(1.))
+        self.assertTrue(self.grid.has(1.5))
+        self.assertTrue(self.grid.has(2.))
+        self.assertFalse(self.grid.has(2.1))
+
+    def test_lower_bound(self):
+        self.assertAlmostEqual(self.grid.lower_bound, 1.)
+
+    def test_upper_bound(self):
+        self.assertAlmostEqual(self.grid.upper_bound, 2.)
+
+
+class TestSubGrid1D(unittest.TestCase):
+
+    def setUp(self):
+        self.parent_grid = kratos_inlet_process.RegularGrid1D(0., 1., 11)
+        self.sub_grid = kratos_inlet_process.SubGrid1D(self.parent_grid, 0.21, 0.79)
+
+    def test_getitem(self):
+        self.assertAlmostEqual(self.sub_grid[0], self.parent_grid[2])
+        self.assertAlmostEqual(self.sub_grid[1], self.parent_grid[3])
+
+    def test_len(self):
+        self.assertEqual(len(self.sub_grid), 7)
+
+    def test_has(self):
+        self.assertFalse(self.sub_grid.has(0.19))
+        self.assertTrue(self.sub_grid.has(0.2))
+        self.assertTrue(self.sub_grid.has(0.5))
+        self.assertTrue(self.sub_grid.has(0.8))
+        self.assertFalse(self.sub_grid.has(0.81))
+
+    def test_floor_index(self):
+        self.assertEqual(self.sub_grid.floor_index(0.199999), 0)
+        self.assertEqual(self.sub_grid.floor_index(0.35), 1)
+
+    def test_ceil_index(self):
+        self.assertEqual(self.sub_grid.ceil_index(0.800001), 6)
+        self.assertEqual(self.sub_grid.ceil_index(0.65), 5)
+
+    def test_lower_bound(self):
+        self.assertAlmostEqual(self.sub_grid.lower_bound, 0.2)
+
+    def test_upper_bound(self):
+        self.assertAlmostEqual(self.sub_grid.upper_bound, 0.8)
+
+    def test_slice(self):
+        self.assertEqual(self.sub_grid.gslice, slice(2, 9))
+
+
+class TestInletPanel3D(unittest.TestCase):
+
+    def setUp(self):
+        grid_x = kratos_inlet_process.RegularGrid1D(0., 2., 3)
+        grid_y = kratos_inlet_process.RegularGrid1D(1., 1., 2)
+        grid_z = kratos_inlet_process.RegularGrid1D(1., 1., 2)
+        sub_grid_y = kratos_inlet_process.SubGrid1D(grid_y, 1., 2.)
+        sub_grid_z = kratos_inlet_process.SubGrid1D(grid_z, 1., 2.)
+        grid = kratos_inlet_process.Grid3D(grid_x, sub_grid_y, sub_grid_z)
+        data = array(
+            (
+                ((0.2, 0.6), (0.1, 0.4)),
+                ((0.7, 0.5), (0.2, 0.8)),
+                ((0.4, 0.4), (0.5, 0.6))
+            )
+        )
+        self.panel = kratos_inlet_process.InletPanel3D(grid, data)
+
+    def test_update(self):
+        self.panel.update(0.)
+        self.assertAlmostEqual(self.panel.cut_data[0, 0], 0.2)
+        self.assertAlmostEqual(self.panel.cut_data[0, 1], 0.6)
+        self.assertAlmostEqual(self.panel.cut_data[1, 0], 0.1)
+        self.assertAlmostEqual(self.panel.cut_data[1, 1], 0.4)
+        self.panel.update(0.5)
+        self.assertAlmostEqual(self.panel.cut_data[0, 0], 0.45)
+        self.assertAlmostEqual(self.panel.cut_data[0, 1], 0.55)
+        self.assertAlmostEqual(self.panel.cut_data[1, 0], 0.15)
+        self.assertAlmostEqual(self.panel.cut_data[1, 1], 0.6)
+        self.panel.update(1.)
+        self.assertAlmostEqual(self.panel.cut_data[0, 0], 0.7)
+        self.assertAlmostEqual(self.panel.cut_data[0, 1], 0.5)
+        self.assertAlmostEqual(self.panel.cut_data[1, 0], 0.2)
+        self.assertAlmostEqual(self.panel.cut_data[1, 1], 0.8)
+        self.panel.update(1.5)
+        self.assertAlmostEqual(self.panel.cut_data[0, 0], 0.55)
+        self.assertAlmostEqual(self.panel.cut_data[0, 1], 0.45)
+        self.assertAlmostEqual(self.panel.cut_data[1, 0], 0.35)
+        self.assertAlmostEqual(self.panel.cut_data[1, 1], 0.7)
+
+    def test_interpolate(self):
+        self.panel.update(0.)
+        self.assertAlmostEqual(self.panel.interpolate(MagicMock(Y=1., Z=1.)), 0.2)
+        self.assertAlmostEqual(self.panel.interpolate(MagicMock(Y=1.5, Z=1.)), 0.15)
+        self.assertAlmostEqual(self.panel.interpolate(MagicMock(Y=1., Z=1.5)), 0.4)
+        self.assertAlmostEqual(self.panel.interpolate(MagicMock(Y=1.5, Z=1.5)), 0.325)
+    
+
+class TestLogMeanProfile(unittest.TestCase):
+
+    def setUp(self):
+        self.mean_profile = kratos_inlet_process.LogMeanProfile(0.59345, 0.02, 15.)
+
+    def test_wind_speed(self):
+        self.assertAlmostEqual(self.mean_profile.wind_speed(MagicMock(Z=0.)), 0.)
+        self.assertAlmostEqual(self.mean_profile.wind_speed(MagicMock(Z=20.)), 10., places=3)
+
+    def test_bulk_wind_speed(self):
+        self.assertAlmostEqual(self.mean_profile.bulk_wind_speed, 15.)
+
+
+class TestImposeWindInletProcess(unittest.TestCase):
+
+    data = array(
+        (
+            ((0.2,-0.1), (0.1, 0.0)),
+            ((0.2, 0.0), (0.4,-0.2)),
+            ((0.0, 0.1), (0.1, 0.1))
+        )
+    )
+
+    class MockFile(dict, AbstractContextManager):
+        pass
+
+    def setUp(self):
+        self.settings = {}
+        self.settings['inlet_model_part_name'] = 'inlet'
+        self.settings['inlet_position'] = 0.
+        self.settings['wind_filename'] = 'wind.h5'
+        self.settings['ramp_time'] = 10.
+        self.settings['y0'] = 0.
+        self.settings['z0'] = 0.
+        self.model = {'inlet':
+                      MagicMock(
+                          Nodes=[
+                              MagicMock(X=0.5, Y=0.5, Z=0.0)
+                          ],
+                          ProcessInfo={
+                              'DELTA_TIME': 0.1,
+                              'TIME': 0.0
+                          }
+                      )
+        }
+        self.mock_file = self.MockFile()
+        self.mock_file['lx'] = (2.,)
+        self.mock_file['ly'] = (1.,)
+        self.mock_file['lz'] = (1.,)
+        self.mock_file['log_z0'] = (0.02,)
+        self.mock_file['height'] = (20.,)
+        self.mock_file['umean'] = (12.3,)
+        self.mock_file['u'] = self.data
+        self.mock_file['v'] = self.data
+        self.mock_file['w'] = self.data
+
+    def test_init(self):
+        with patch('kratos_inlet_process.ImposeWindInletProcess.OpenFile') as file_:
+            file_.return_value = self.mock_file
+            process = kratos_inlet_process.ImposeWindInletProcess(
+                self.model, self.settings)
+            self.assertEqual(process.inlet_model_part_name, 'inlet')
+            self.assertEqual(process.wind_filename, 'wind.h5')
+            self.assertAlmostEqual(process.inlet_position, 0.)
+            self.assertAlmostEqual(process.ramp_time, 10.)
+            self.assertAlmostEqual(process.lx, 2.)
+            self.assertAlmostEqual(process.ly, 1.)
+            self.assertAlmostEqual(process.lz, 1.)
+            self.assertAlmostEqual(process.log_z0, 0.02)
+            self.assertAlmostEqual(process.height, 20.)
+            self.assertAlmostEqual(process.umean, 12.3)
+            self.assertEqual(len(process.model_part.Nodes), 1)
+            
+    def test_create_log_mean_profile(self):
+        with patch('kratos_inlet_process.ImposeWindInletProcess.OpenFile') as file_:
+            file_.return_value = self.mock_file
+            process = kratos_inlet_process.ImposeWindInletProcess(
+                self.model, self.settings)
+            mean_profile = process.CreateLogMeanProfile()
+            node = MagicMock(Z=20.)
+            self.assertAlmostEqual(mean_profile.wind_speed(node), 12.3, places=2)
+    
+    def test_update_inlet_position(self):
+        with patch('kratos_inlet_process.ImposeWindInletProcess.OpenFile') as file_:
+            file_.return_value = self.mock_file
+            process = kratos_inlet_process.ImposeWindInletProcess(
+                self.model, self.settings)
+            process.mean_profile.bulk_wind_speed = 10.0
+            process.UpdateInletPosition()
+            self.assertAlmostEqual(process.inlet_position, 1.0)
+            for i in range(2):
+                process.UpdateInletPosition()
+            self.assertAlmostEqual(process.inlet_position, 1.0)
+
+    def test_create_3d_mappers(self):
+        with patch('kratos_inlet_process.ImposeWindInletProcess.OpenFile') as file_:
+            file_.return_value = self.mock_file
+            process = kratos_inlet_process.ImposeWindInletProcess(
+                self.model, self.settings)
+            self.assertEqual(process.mappers[0][0], 'VELOCITY_X')
+            self.assertEqual(process.mappers[1][0], 'VELOCITY_Y')
+            self.assertEqual(process.mappers[2][0], 'VELOCITY_Z')
+            mapper_u = process.mappers[0][1]
+            mapper_u.update(process.inlet_position)
+            self.assertAlmostEqual(mapper_u.interpolate(MagicMock(Y=0.5, Z=0.5)), 0.05)
+            mapper_u.update(0.5)
+            self.assertAlmostEqual(mapper_u.interpolate(MagicMock(Y=0.5, Z=0.5)), 0.075)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
