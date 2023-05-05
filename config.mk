6420f109b910ef64fcbe0cbdb9524a6204ed0dea
Mate Pentek <mpentek@gmail.com>
2023-05-05 11:57:50 +0200

    Updating config and Makefile for fftw-3.3.10

--- config.mk
+++ config.mk
@@ -1,31 +1,32 @@
-WFS_ROOT              := /software/user/windgen
+WFS_ROOT              := <path_to_repository>/WindGen
 
 # ----------------------------------------------------------------------
-#				NO MPI
+#                               NO MPI
 # ----------------------------------------------------------------------
-CC                    := gcc 
-FFTW_CONFIG_FLAGS     := --prefix=$(WFS_ROOT)/fftw-3.3.3/
-CFLAGS                := -O2 -ffast-math -freciprocal-math -I $(WFS_ROOT)/src -I $(WFS_ROOT)/fftw-3.3.3/include
-LDFLAGS               := -L $(WFS_ROOT)/src -L $(WFS_ROOT)/fftw-3.3.3/lib
+CC                    := gcc-11
+FFTW_CONFIG_FLAGS     := --prefix=$(WFS_ROOT)/fftw-3.3.10/
+CFLAGS                := -O2 -ffast-math -freciprocal-math -I $(WFS_ROOT)/src -I $(WFS_ROOT)/fftw-3.3.10/include
+LDFLAGS               := -L $(WFS_ROOT)/src -L $(WFS_ROOT)/fftw-3.3.10/lib
 LDFLAGS               += -lwfs -lfftw3
 
 # ----------------------------------------------------------------------
 #				MPI
 # ----------------------------------------------------------------------
 #CC                    := mpicc 
-#FFTW_CONFIG_FLAGS     := --enable-mpi --prefix=$(WFS_ROOT)/fftw-3.3.3/
-#CFLAGS                := -DHAVE_MPI -O0 -I $(WFS_ROOT)/src -I $(WFS_ROOT)/fftw-3.3.3/include
-#LDFLAGS               := -L $(WFS_ROOT)/src -L $(WFS_ROOT)/fftw-3.3.3/lib
+#FFTW_CONFIG_FLAGS     := --enable-mpi --prefix=$(WFS_ROOT)/fftw-3.3.10/
+#CFLAGS                := -DHAVE_MPI -O2 -ffast-math -freciprocal-math -I $(WFS_ROOT)/src -I $(WFS_ROOT)/fftw-3.3.10/include
+#LDFLAGS               := -L $(WFS_ROOT)/src -L $(WFS_ROOT)/fftw-3.3.10/lib
 #LDFLAGS               += -lwfs -lfftw3_mpi -lfftw3
 
 # ----------------------------------------------------------------------
-#				HDF5
+#                               HDF5
 # ----------------------------------------------------------------------
-#HDF5_ROOT              := /software/user/HDF5/HDF5
-#ZLIB_ROOT              := /software/user/HDF5/zlib
-#SZIP_ROOT              := /software/user/HDF5/szip
-#CFLAGS                 += -DHAVE_HDF5 -I $(HDF5_ROOT)/include
-#LDFLAGS                += $(HDF5_ROOT)/lib/libhdf5.a $(ZLIB_ROOT)/lib/libz.a $(SZIP_ROOT)/lib/libsz.a
+HDF5_ROOT              := /usr/lib/x86_64-linux-gnu
+ZLIB_ROOT              := /usr/lib/x86_64-linux-gnu
+SZIP_ROOT              := /usr/lib/x86_64-linux-gnu
+CFLAGS                 += -I/usr/include/hdf5/serial -DHAVE_HDF5
+LDFLAGS                += $(HDF5_ROOT)/hdf5/serial/libhdf5.a
 # ----------------------------------------------------------------------
 
-LDFLAGS                += -lm
+LDFLAGS                += -pthread -lsz -lz -ldl -lm
+
